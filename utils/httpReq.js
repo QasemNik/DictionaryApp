f5c93608a1ec4e82582146340a03cd7752ba831c
Ghasem Nikzad <ghasem295@gmail.com>
2024-07-10 21:46:34 +0330

    commit

--- utils/httpReq.js
+++ utils/httpReq.js
@@ -1,13 +1,19 @@
-const URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+const URL = 'https://api.dictionaryapi.dev/api/v2/entries/n/';
 
 const fetchData = async (word) => {
     try {
         const res = await fetch(`${URL} ${word}`);
         const json = await res.json(); 
-        return json   
-          
+        if(!res.ok){
+            swal(`No results found for'${word}'`,"The word you've entered isn't in the dictionary." , "warning");
+            
+        } else{
+            return json;
+
+        }
     } catch (error) {
-        console.log('Error' + error);
+        swal('Error',"Something went wrong" , "error");
+        ;
     }
 }
 
